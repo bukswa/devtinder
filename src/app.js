ec111c7291dcf094a541ed58b8e19d1866d2a44a
Aqeel <bukswa@gmail.com>
2024-11-21 18:01:51 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -3,12 +3,36 @@ const app = express();
 
 const PORT = 7777;
 
-app.use("/test", (req, res) => {
-  res.send("Test Page: Hello from the server!");
+// app.use("/", (req, res) => {
+//   res.send("Home page");
+// });
+
+// app.use("/hello", (req, res) => {
+//     res.send("Hello Page: Hello from the server!");
+//   });
+
+// app.use("/test/2", (req, res) => {
+//   res.send("Test Page: Sub route 2");
+// });
+
+// app.use will match all the HTTP methods to the route
+// where as app.get will only match HTTP:GET method
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "aqeel", lastName: "ahmad" });
 });
 
-app.use("/", (req, res) => {
-  res.send("Home page");
+app.post("/user", (req, res) => {
+  console.log("save the data to database");
+  res.send("successfully saved");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Test Page: Main Route");
 });
 
 app.listen(PORT, () => {
