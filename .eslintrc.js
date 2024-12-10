60af6bcbeec9bbf87c0ff19382f68fe6431150e2
Aqeel <bukswa@gmail.com>
2024-12-10 17:02:28 +0530

    eslint added to project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// .eslintrc.js
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-unused-vars": "error",
+    "no-undef": "error",
+  },
+};
